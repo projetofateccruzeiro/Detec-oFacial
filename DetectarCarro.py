adc5589711fa457f3edef6ae7108124dd727adc8
jmauricio2019 <54713911+jmauricio2019@users.noreply.github.com>
2020-01-07 11:03:55 -0300

    Update DetectarCarro.py

--- DetectarCarro.py
+++ DetectarCarro.py
@@ -2,7 +2,7 @@ import cv2
 
 classificador = cv2.CascadeClassifier('cascades\\cars.xml')
 
-imagem = cv2.imread("outros\\carro1.jpg")
+imagem = cv2.imread("outros\\carro3.jpg")
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
 detectado = classificador.detectMultiScale(imagemCinza, scaleFactor=1.01)
@@ -10,7 +10,7 @@ print(detectado)
 
 for(x,y, l, a)in detectado:
     imagem = cv2.rectangle(imagem,(x,y),(x + l, y + a),(0, 0, 255),2 )
-    if(l>50 or a >50):#se o tamanho do veiculo for maior definido pelo cliente
+    if(l>100 or a >100):#se o tamanho do veiculo for maior definido pelo cliente
         print("Fora do Padrão ")# Exibe está mensagem
 
 cv2.imshow("Encontrado", imagem)
